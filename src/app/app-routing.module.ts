0fa7bc805ff3cf849a42d17fbe041e21b760dc95
git@github.com:kilmatic/karsAreUs.git <kilakanya@gmail.com>
2023-04-09 10:51:37 +0200

    Dashboard build in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,18 +10,19 @@ import { PharmacistComponent } from './dashboard/pharmacist/pharmacist.component
 import { ReceptionistComponent } from './dashboard/receptionist/receptionist.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'accountCreator', component: AccountCreatorComponent },
-  { path: 'doctor', component: DoctorComponent },
-  { path: 'nurse', component: NurseComponent },
-  { path: 'patient', component: PatientComponent },
-  { path: 'pharmacist', component: PharmacistComponent },
-  { path: 'receptionist', component: ReceptionistComponent },
-  { path: 'administrator', component: AdministratorComponent },
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'accountCreator', component: AccountCreatorComponent, canActivate: [AuthGuardService] },
+  { path: 'doctor', component: DoctorComponent, canActivate: [AuthGuardService] },
+  { path: 'nurse', component: NurseComponent, canActivate: [AuthGuardService] },
+  { path: 'patient', component: PatientComponent, canActivate: [AuthGuardService] },
+  { path: 'pharmacist', component: PharmacistComponent, canActivate: [AuthGuardService] },
+  { path: 'receptionist', component: ReceptionistComponent, canActivate: [AuthGuardService] },
+  { path: 'administrator', component: AdministratorComponent, canActivate: [AuthGuardService] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardService] }
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  sidebar:any = document.querySelector("#sidebar");
+  maxSidebar:any = document.querySelector(".max");
+  miniSidebar:any = document.querySelector(".mini");
+  roundout:any = document.querySelector(".roundout");
+  maxToolbar:any = document.querySelector(".max-toolbar");
+  logo:any = document.querySelector('.logo');
+  content:any = document.querySelector('.content');
+  moon:any = document.querySelector(".moon");
+  sun:any = document.querySelector(".sun");
+
+  constructor( ) { }
 
   ngOnInit(): void {
+    this.openNav();
   }
 
+  openNav() {
+    if(this.sidebar.classList.contains('-translate-x-48')){
+      this.sidebar.classList.remove("-translate-x-48")
+      this.sidebar.classList.add("translate-x-none")
+      this.maxSidebar.classList.remove("hidden")
+      this.maxSidebar.classList.add("flex")
+      this.miniSidebar.classList.remove("flex")
+      this.miniSidebar.classList.add("hidden")
+      this.maxToolbar.classList.add("translate-x-0")
+      this.maxToolbar.classList.remove("translate-x-24","scale-x-0")
+      this.logo.classList.remove("ml-12")
+      this.content.classList.remove("ml-12")
+      this.content.classList.add("ml-12","md:ml-60")
+    }else{
+      this.sidebar.classList.add("-translate-x-48")
+      this.sidebar.classList.remove("translate-x-none")
+      this.maxSidebar.classList.add("hidden")
+      this.maxSidebar.classList.remove("flex")
+      this.miniSidebar.classList.add("flex")
+      this.miniSidebar.classList.remove("hidden")
+      this.maxToolbar.classList.add("translate-x-24","scale-x-0")
+      this.maxToolbar.classList.remove("translate-x-0")
+      this.logo.classList.add('ml-12')
+      this.content.classList.remove("ml-12","md:ml-60")
+      this.content.classList.add("ml-12")
+    }
+  }
+  //   if(sidebar.classList.contains('-translate-x-48')){
+  //       // max sidebar
+  //       sidebar.classList.remove("-translate-x-48")
+  //       sidebar.classList.add("translate-x-none")
+  //       maxSidebar.classList.remove("hidden")
+  //       maxSidebar.classList.add("flex")
+  //       miniSidebar.classList.remove("flex")
+  //       miniSidebar.classList.add("hidden")
+  //       maxToolbar.classList.add("translate-x-0")
+  //       maxToolbar.classList.remove("translate-x-24","scale-x-0")
+  //       logo.classList.remove("ml-12")
+  //       content.classList.remove("ml-12")
+  //       content.classList.add("ml-12","md:ml-60")
+  //   }else{
+  //       // mini sidebar
+  //       sidebar.classList.add("-translate-x-48")
+  //       sidebar.classList.remove("translate-x-none")
+  //       maxSidebar.classList.add("hidden")
+  //       maxSidebar.classList.remove("flex")
+  //       miniSidebar.classList.add("flex")
+  //       miniSidebar.classList.remove("hidden")
+  //       maxToolbar.classList.add("translate-x-24","scale-x-0")
+  //       maxToolbar.classList.remove("translate-x-0")
+  //       logo.classList.add('ml-12')
+  //       content.classList.remove("ml-12","md:ml-60")
+  //       content.classList.add("ml-12")
+  //   }
+  // }
 }
